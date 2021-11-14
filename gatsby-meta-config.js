29bb3f6f21aed39617790e0f3b9bf1f903a5c5c2
Lee,YoungHoon <nobel6018@gmail.com>
2021-11-14 17:13:29 +0900

    feat: hide about link and remove example about items

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `blog.leedo.me`,
+  title: `v2.leedo.me`,
   description: `이도(李裪)`,
   language: `ko`,
   siteUrl: `https://blog.leedo.me/`,
@@ -44,12 +44,14 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '',
+        activity: '',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          github: '',
+          post: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
         },
       },
     ],
@@ -73,15 +75,16 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+        title: '',
+        description: '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
         },
       },
     ],
