338f47218e390c4a7f73d1d9a09f9cc534d43678
Lee,YoungHoon <nobel6018@gmail.com>
2021-11-14 18:22:58 +0900

    fix: GA tracking id

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -9,7 +9,7 @@ module.exports = {
       repo: `nobel6018/nobel6018.github.io`,
     },
   },
-  ga: 'G-7FPPR1L7LZ', // Google Analytics Tracking ID
+  ga: 'UA-212825109-1', // Google Analytics Tracking ID
   author: {
     name: `이영훈`,
     bio: {
